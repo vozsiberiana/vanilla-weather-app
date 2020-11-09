25030fb2aa6bf41fcdb56e2a6a147f1c0ba255ee
vozsiberiana <70288823+vozsiberiana@users.noreply.github.com>
2020-11-09 21:48:40 +0100

    Added description, wind, humidity

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log("Hello world");
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  city.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "0b1fa8952349f479be72640e8d64dd95";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
